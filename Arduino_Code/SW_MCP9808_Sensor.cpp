e76c1b95d200cfb56981ec68a8387050236e5686
StoicWeatherWatch <34807600+StoicWeatherWatch@users.noreply.github.com>
2018-02-19 22:31:56 -0800

    Update SW_MCP9808_Sensor.cpp
    Not a clue

--- Arduino_Code/SW_MCP9808_Sensor.cpp
+++ Arduino_Code/SW_MCP9808_Sensor.cpp
@@ -1,182 +1,58 @@
 /*
- * SW_MCP9808_Sensor.cpp
+ * SW_MCP9808_Sensor.h
  *
  *  Created on: 2018-01-21
  *      Author: StoicWeather
+ *
+ *      For maximum temperature resolution this sensor takes 250 ms to reach conversion. Res +0.0625°C
  */
 
-#include "SW_MCP9808_Sensor.h"
+#ifndef SW_MCP9808_SENSOR_H_
+#define SW_MCP9808_SENSOR_H_
 
-SW_MCP9808_Sensor::SW_MCP9808_Sensor(byte AddressIn, I2C I2CBussIn, byte SensorNumberIN)
-:SW_Sensor(AddressIn,I2CBussIn, SensorNumberIN)
-{
+#include "SWSensor.h"
+#include "SW_Helper_Functions.h"
 
+#define MCP9808_CHIPID_REG    0x07
+#define MCP9808_MANID_REG     0x06
+#define MCP9808_RES_REG       0x08
+#define MCP9808_CONFIG_REG    0x01
+#define MCP9808_TEMP_REG      0x05
 
-}
+#define MCP9808_CHIPID_VALUE  0x0400
+#define MCP9808_MANID_VALUE   0x0054
 
-bool SW_MCP9808_Sensor::InitializeSensor()
-{
-
-	Serial.print(F("#Initializing MCP9808 Sensor "));
-	Serial.print((int)SensorNumber);
-	Serial.println(" ;");
-
-#ifdef VERIFY_CHIPS
-	if(!VerifyChip())
-	{
-		return false;
-	}
-#endif /*VERIFY_CHIPS*/
-
-	// Set for maximum temperature resolution
-	I2CBuss.write(SensorAddress, (byte)MCP9808_RES_REG, (byte)MCP9808_MAXRES_CMD);
-
-	byte Configuration[2];
-	Configuration[0] = (byte)MCP9808_CONFIG_MSB_CMD;
-	Configuration[1] = (byte)MCP9808_CONFIG_LSB_CMD;
-	I2CBuss.write(SensorAddress, (byte)MCP9808_CONFIG_REG, Configuration, (byte)2);
+#define MCP9808_MAXRES_CMD    0x03
+//0000 0000 0000 0000
+#define MCP9808_CONFIG_MSB_CMD    0x00
+#define MCP9808_CONFIG_LSB_CMD    0x00
 
 
-	return true;
-}
 
 /*
- * This will read data into the data variable. It returns true on success. A false return is less certain.
+ *
  */
-bool SW_MCP9808_Sensor::AcquireData()
+class SW_MCP9808_Sensor: public SW_Sensor
 {
-	I2CBuss.read(SensorAddress, (byte)MCP9808_TEMP_REG, (byte)2, TemperatureRaw);
+	protected:
+	byte TemperatureRaw[2];
 
-	//Serial.println(TemperatureRaw[0],HEX);
-	//Serial.println(TemperatureRaw[1],HEX);
+	public:
 
+	SW_MCP9808_Sensor(byte AddressIn, I2C I2CBussIn, byte SensorNumberIN);
+	bool InitializeSensor();
+	bool AcquireData();
+	bool SendRawDataSerial();
 
-	return true;
-}
+	byte GetRawTempreature_HighBits();
+	byte GetRawTempreature_LowBits();
 
-bool SW_MCP9808_Sensor::SendRawDataSerial()
-{
-	Serial.print(F("*"));
-	Serial.print(SensorNumber,DEC);
-	Serial.print(F("T,"));
-	SerialHexBytePrint(TemperatureRaw[0]);
-	SerialHexBytePrint(TemperatureRaw[1]);
-	//Serial.print(TemperatureRaw[0],HEX);
-	//Serial.print(TemperatureRaw[1],HEX);
-	Serial.println(F(";"));
-
-	return true;
-}
-
-/*  Unused
-byte SW_MCP9808_Sensor::GetRawTempreature_HighBits()
-{
-	return TemperatureRaw[0];
-}
-byte SW_MCP9808_Sensor::GetRawTempreature_LowBits()
-{
-	return TemperatureRaw[1];
-}
-*/
-
-#ifdef PERFORM_DATA_PROCESSING
-	float SW_MCP9808_Sensor::ProcessTemp()
-	{
-		float T = 0;
-
-		// TODO choose a method
-
-		//Method 1
-		int shift = 3;
-		for (int i = 7; i >= 4; i--)
-		{
-			 T += pow(2.0,(float)i) * ((TemperatureRaw[0] >> shift) & (byte)1);
-			 shift--;
-		}
-		shift = 7;
-		for (int i = 3; i >= -4; i--)
-		{
-			T += pow(2.0,(float)i) * ((TemperatureRaw[1] >> shift) & (byte)1);
-			shift--;
-		}
-
-		// Negative if true
-		if((TemperatureRaw[0] >> 4) & (byte)1)
-		{
-			T = -1.0*T;
-		}
-
-		//Method 2
-		if(!((TemperatureRaw[0] >> 4) & (byte)1))
-		{
-			T = (float)(TemperatureRaw[0] & (byte)0x0F) * pow(2.0,4.0);
-			T += (float)(TemperatureRaw[1]) * pow(2.0,-4.0);
-
-		}
-		else
-		{
-			T = 256.0 - (float)(TemperatureRaw[0] & (byte)0x0F) * pow(2.0,4.0);
-			T = T - (float)(TemperatureRaw[1]) * pow(2.0,-4.0);
-		}
-
-
-
-		return T;
-
-	}
-#endif /*PERFORM_DATA_PROCESSING*/
-
-
-// Verify that the chip we are talking to is correct.
 #ifdef VERIFY_CHIPS
-	bool SW_MCP9808_Sensor::VerifyChip()
-	{
-		// Get Sensor ID and check it
-		byte status = I2CBuss.read(SensorAddress, (byte)MCP9808_MANID_REG, (byte)2);
-
-		int DataIn = (int)I2CBuss.receive();
-		// Most sig bits came in first. They get shifted up.
-		DataIn <<= 8;
-		DataIn |= (int)I2CBuss.receive();
-
-		//Serial.println(status);
-		//Serial.println(DataIn,HEX);
-
-		Serial.print(F("#VerifyChip MCP9808 Sensor "));
-		Serial.print((int)SensorNumber);
-		Serial.println(" ;");
-
-		if(DataIn == (int)MCP9808_MANID_VALUE)
-		{
-			Serial.println(F("#MCP9808 Correct Man ID;"));
-		}
-		else
-		{
-			Serial.println(F("#NOT MCP9808 Correct Man ID;"));
-			return false;
-		}
-
-		status = I2CBuss.read(SensorAddress, (byte)MCP9808_CHIPID_REG, (byte)2);
-
-		DataIn = (int)I2CBuss.receive();
-		// Most sig bits came in first. They get shifted up.
-		DataIn <<= 8;
-		DataIn |= (int)I2CBuss.receive();
-
-		//Serial.println(status);
-		//Serial.println(DataIn,HEX);
-
-		if(DataIn == (int)MCP9808_CHIPID_VALUE)
-		{
-			Serial.println(F("#Correct Chip;"));
-		}
-		else
-		{
-			Serial.println(F("#NOT Correct Chip;"));
-			return false;
-		}
-
-
-		return true;
-	}
+	bool VerifyChip();
 #endif /*VERIFY_CHIPS*/
+#ifdef PERFORM_DATA_PROCESSING
+	float ProcessTemp();
+#endif /*PERFORM_DATA_PROCESSING*/
+};
+
+#endif /* SW_MCP9808_SENSOR_H_ */
