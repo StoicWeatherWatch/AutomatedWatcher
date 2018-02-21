6b0c70d8446ff30ac2e511cb70724237db9f2f2e
StoicWeatherWatch <NotAnEmail@NotAServer.NotATopLevelDomain>
2018-02-21 00:45:35 -0800

    Revert "Take it"
    This reverts commit a629e3ee481d51641cfc2c8f2fe289fa40971571.

--- Arduino_Code/sloeber.ino.cpp
+++ Arduino_Code/sloeber.ino.cpp
@@ -2,15 +2,24 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-02-21 00:20:06
+//This file has been generated on 2018-01-10 01:11:28
 
 #include "Arduino.h"
-#include "SW_Conditional_Includes.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 void setup() ;
 void loop() ;
+void displaySensorDetails(void) ;
+void configureSensor(void) ;
+void setup(void)  ;
+void loop(void)  ;
 
 #include "StoicWatcher.ino"
 
+#include "sensorapi_JW.ino"
 
 #endif
