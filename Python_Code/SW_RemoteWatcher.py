8e12ac14ff50c654a80bcbc0c988a27116b5e612
StoicWeatherWatch <34807600+StoicWeatherWatch@users.noreply.github.com>
2019-06-19 22:19:17 -0700

    Indentation error

--- Python_Code/SW_RemoteWatcher.py
+++ Python_Code/SW_RemoteWatcher.py
@@ -118,17 +118,17 @@ class SW_RemoteWatcher(object):
     def StopMonitoringForRemotes(self):
         loginf("SW_RemoteWatcher StopMonitoringForRemotes called but has issues - might want to fix this someday")
         #TODO Make this work
-         try:
-             loginf("SW_RemoteWatcher StopMonitoringForRemotes Running")
-             reactor.callFromThread(reactor.stop)
-             loginf("SW_RemoteWatcher StopMonitoringForRemotes Done")
-             return True
-         except:
-             loginf("SW_RemoteWatcher StopMonitoringForRemotes some error")
-             loginf(traceback.format_exc())
-             #This kills the thread brutaly
-             self.ThreadForTwisted.exit()
-             return False
+        try:
+            loginf("SW_RemoteWatcher StopMonitoringForRemotes Running")
+            reactor.callFromThread(reactor.stop)
+            loginf("SW_RemoteWatcher StopMonitoringForRemotes Done")
+            return True
+        except:
+            loginf("SW_RemoteWatcher StopMonitoringForRemotes some error")
+            loginf(traceback.format_exc())
+            #This kills the thread brutaly
+            self.ThreadForTwisted.exit()
+            return False
     
     def ReportTheDataToWeeWX(self, data):
         """
