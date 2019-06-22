525eec6c1050c072fda15c71e80429dc53aaea99
StoicWeatherWatch <34807600+StoicWeatherWatch@users.noreply.github.com>
2019-06-22 16:25:04 -0700

    Update SW_Engine.py

--- Python_Code/SW_Engine.py
+++ Python_Code/SW_Engine.py
@@ -15,20 +15,20 @@ import syslog
 
 def logmsg(level, msg):
     try:
-	    syslog.syslog(level, 'SW_Engine: %s' % msg)
+        syslog.syslog(level, 'SW_Engine: %s' % msg)
     except TypeError as detail:
-	# This happened onece on daemon stop. Well into a stop that seemed to be going well,
-	#  a line was read in from serial and sent to loginf. Syslog spit out a TypeError
-	#  and the program crashed and would not come back to life automatically.
-	    syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: syslog spit a TypeError exception")
-	    syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: Usually means string input has issues or is none")
-	    syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: TypeError: %s" % detail)
-	    syslog.syslog(syslog.LOG_ERR,"SW_Engine: Traceback: \n%s" % traceback.format_exc())
-	    syslog.syslog(syslog.LOG_INFO, "SW_Engine: Stoic will now try to put this behind it and continue with life")
+        # This happened onece on daemon stop. Well into a stop that seemed to be going well,
+        #  a line was read in from serial and sent to loginf. Syslog spit out a TypeError
+        #  and the program crashed and would not come back to life automatically.
+        syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: syslog spit a TypeError exception")
+        syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: Usually means string input has issues or is none")
+        syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: TypeError: %s" % detail)
+        syslog.syslog(syslog.LOG_ERR,"SW_Engine: Traceback: \n%s" % traceback.format_exc())
+        syslog.syslog(syslog.LOG_INFO, "SW_Engine: Stoic will now try to put this behind it and continue with life")
     except:
-	    syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: syslog spit an error other than TypeError exception. (Raised to next level)")
-	    syslog.syslog(syslog.LOG_ERR,"SW_Engine: Traceback: \n%s" % traceback.format_exc())
-	raise
+        syslog.syslog(syslog.LOG_ERR, "SW_Engine: ERROR: syslog spit an error other than TypeError exception. (Raised to next level)")
+        syslog.syslog(syslog.LOG_ERR,"SW_Engine: Traceback: \n%s" % traceback.format_exc())
+        raise
 
 def logdbg(msg):
     logmsg(syslog.LOG_DEBUG, msg)
